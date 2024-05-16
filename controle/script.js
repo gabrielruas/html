2ac27194fc39d99f748f85262f8c010a32ef9b27
Gabriel Ruas <gabrielruas30@gmail.com>
2024-05-16 19:16:23 -0300

    projeto controle

---
+++ controle/script.js
@@ -0,0 +1,15 @@
+// Função para lidar com o clique nos botões
+function funcaoDoJavaScript(acao) {
+    if (acao === 'frente') {
+        // Se o botão "frente" for clicado
+        alert('Você clicou no botão "frente"!');
+        // Adicione aqui o código que deseja executar para a ação "frente"
+    } else if (acao === 're') {
+        // Se o botão "re" for clicado
+        alert('Você clicou no botão "re"!');
+        // Adicione aqui o código que deseja executar para a ação "re"
+    } else {
+        // Caso algum outro valor seja passado
+        alert('Opção inválida!');
+    }
+}
